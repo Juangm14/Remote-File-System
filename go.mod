1439d19c12efdffc0b3f132c6111228c2365d2ee
Juan <jgm179@alu.ua.es>
2022-05-27 22:10:10 +0200

    Prueba temporal

--- go.mod
+++
@@ -1,8 +0,0 @@
-module lajr1dominio.com/proy
-
-go 1.18
-
-require (
-	github.com/mattn/go-sqlite3 v1.14.12 // indirect
-	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
-)
--- servidortls.go
+++ servidortls.go
@@ -12,10 +12,38 @@ import (
 	"strconv"
 
 	"golang.org/x/crypto/pbkdf2"
-	. "lajr1dominio.com/proy/structs"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type DataS struct {
+	Action int   `json:"action"`
+	User   UserS `json:"user"`
+}
+type UserS struct {
+	Name     []byte `json:"name"`
+	Password []byte `json:"password"`
+	Archivo  FileS  `json:"archivo"`
+}
+
+type FileS struct {
+	NameFile []byte `json:"nameFile"`
+	NameH    []byte `json:"nameH"`
+	Peso     int    `json:"peso"`
+	Data     []byte `json:"data"`
+	Token    []byte `json:"token"`
+	Id       int
+	Version  int
+}
+
+type RespuestaS struct {
+	ActAny int
+	ErrNum int
+	Msg    string
+	File   FileS
+	Data   []byte
+	Files  []FileS
+}
 
 // Meter tambien en la base de datos el nombre hasheado
 func checkErrorServer(e error) {
